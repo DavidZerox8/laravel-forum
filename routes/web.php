1a22863a7b3f9d78195b3c42c0409f537ff689b7
Dev DM <dev_dm@freaks.team>
2023-07-20 15:02:23 -0600

    Livewire installation and first component creation [Class #5]

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Livewire\ShowThreads;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,10 +12,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->group(function ()
+{
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::get('/dashboard/threads', ShowThreads::class)->name('profile.password');
 });
 
 require __DIR__.'/auth.php';
